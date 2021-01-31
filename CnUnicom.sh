34071f981e93c84b7c55a3f19f92829e505afba5
FKDD23 <44658114+FKDD23@users.noreply.github.com>
2021-01-31 16:30:26 +0800

    Update CnUnicom.sh

--- CnUnicom.sh
+++ CnUnicom.sh
@@ -211,14 +211,6 @@ function membercenter() {
     echo; echo membercenter accomplished.
 }
 
-function tgbotinfo() {
-    # TG_BOT通知消息: 未设置相应传入参数时不执行,传入参数格式 token@*** chat_id@*** | 参考: https://github.com/LXK9301/jd_scripts/blob/master/backUp/TG_PUSH.md
-    echo ${all_parameter[*]} | grep -qE "token@[a-zA-Z0-9:_-]+" && token="$(echo ${all_parameter[*]} | grep -oE "token@[a-zA-Z0-9:_-]+" | cut -f2 -d@)" || return
-    echo ${all_parameter[*]} | grep -qE "chat_id@[0-9-]+" && chat_id="$(echo ${all_parameter[*]} | grep -oE "chat_id@[0-9-]+" | cut -f2 -d@)" || return
-    text="$(echo ${userlogin_err[*]} ${#userlogin_err[*]} Failed. ${userlogin_ook[*]} ${#userlogin_ook[*]} Accomplished.)"
-    curl -sX POST "https://api.telegram.org/bot$token/sendMessage" -d "chat_id=$chat_id&text=$text" >/dev/null
-}
-
 function liulactive() {
     # 流量激活功能
     echo ${all_parameter[*]} | grep -qE "liulactive@[mwd]@[0-9a-z]+" || return
@@ -252,8 +244,6 @@ function main() {
         #rm -rf $workdir
     done
     echo; echo $(date) ${userlogin_err[*]} ${#userlogin_err[*]} Failed. ${userlogin_ook[*]} ${#userlogin_ook[*]} Accomplished.
-    # TG通知
-    tgbotinfo
 }
 
 main
